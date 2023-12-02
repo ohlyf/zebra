a424a0c100df39ce2fcedf802af26581b369776b
李云富 <liyunfu1998@qq.com>
2023-12-02 09:41:03 +0800

    feat: 初始化项目

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
