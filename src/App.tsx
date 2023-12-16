cab6c87126791a5f8f1b6f54ad7b8d3d474bfb43
liyunfu <liyunfu1998@qq.com>
2023-12-16 20:35:21 +0800

    feat: 构建路由导航栈

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
 import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
-import TextDemo from './demo/TextDemo';
+import {StatusBar} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {NavigationContainer} from '@react-navigation/native';
+import {TransitionPresets, createStackNavigator} from '@react-navigation/stack';
+
+import PageA from './modules/PageA';
+import PageB from './modules/PageB';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <StatusBar />
-      <TextDemo />
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <StatusBar barStyle={'dark-content'} backgroundColor={'white'} />
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="PageA"
+          screenOptions={{
+            cardStyle: {
+              // view的高度层级 一般必须设置 否则可能出现bug
+              elevation: 1,
+            },
+          }}>
+          <Stack.Screen
+            name="PageA"
+            component={PageA}
+            options={{
+              // TODO
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="PageB"
+            component={PageB}
+            options={{
+              // TODO
+              headerShown: false,
+              ...TransitionPresets.SlideFromRightIOS,
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 
